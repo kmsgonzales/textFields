af501104d462372b30a5acbf52c12f41b5ae0b5e
kmsgonzales <kmsgonzales@comcast.net>
2017-10-12 16:39:54 -0700

    Initial Commit

---
+++ TextFieldsChallengeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextFieldsChallengeApp
+//
+//  Created by Katherine Gonzales on 10/12/17.
+//  Copyright © 2017 KG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
